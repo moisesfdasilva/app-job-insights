9aee6751f1771526d7a4ce2a4f48d1e621366d35
moisesfdasilva <msesfernandes@gmail.com>
2023-04-27 22:16:17 -0300

    feat: desenvolvida a função filter_by_industry que retorna a lista de empregos de um tipo de indústria escolhida, em src/.../industry.py.

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -21,18 +21,4 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
             all_jobs_with_a_industry.append(job)
     return all_jobs_with_a_industry
 
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
     raise NotImplementedError
