7a93f370befc52e22aba6d1735ff0ee956d76cdd
moisesfdasilva <msesfernandes@gmail.com>
2023-04-28 14:43:01 -0300

    test: desenvolvido o teste da função sort_by, verificando se a função ordena conforme os critérios min_salary, max_salary e date_posted, em tests/sorting/test_sorting.py.

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,7 +2,10 @@ from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    "Verifica se a palavra 'Job' aparece 3454 vezes no arquivo data/jobs.csv."
+    """
+        Verifica se a função retorna um dicionário específico do arquivo tests/
+        mocks/brazilians_jobs.csv.
+    """
     output_job = {
         'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
     response = read_brazilian_file(path="tests/mocks/brazilians_jobs.csv")
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,40 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    """
+        Verifica se a função ordena uma lista de dicionários pelo critério
+        min_salary.
+    """
+    input_jobs_list = read(path="data/jobs.csv")
+    sort_by(jobs=input_jobs_list, criteria="min_salary")
+
+    output_job_company = "Fort Worth Independent School District"
+    response_min_salary = read(path="data/jobs.csv")
+
+    assert output_job_company in response_min_salary[0]["company"]
+
+    """
+        Verifica se a função ordena uma lista de dicionários pelo critério
+        max_salary.
+    """
+
+    sort_by(jobs=input_jobs_list, criteria="max_salary")
+
+    output_job_company = "JPMorgan Chase"
+    response_max_salary = read(path="data/jobs.csv")
+
+    assert output_job_company in response_max_salary[0]["company"]
+
+    """
+        Verifica se a função ordena uma lista de dicionários pelo critério
+        date_posted.
+    """
+
+    sort_by(jobs=input_jobs_list, criteria="date_posted")
+
+    output_job_company = "The LiRo Group"
+    response_date_posted = read(path="data/jobs.csv")
+
+    assert output_job_company in response_date_posted[0]["company"]
