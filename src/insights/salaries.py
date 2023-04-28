db57814e9402e7f57dc265de89a618c96d70f26d
moisesfdasilva <msesfernandes@gmail.com>
2023-04-28 11:02:59 -0300

    feat: desenvolvida a função matches_salary_range que retorna "True" se o salário está de acordo com a faixa salarial daquele emprego, caso contrário "False", em src/.../salary.py.

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -41,28 +41,23 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    result = False
+    try:
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+        salary_search = int(salary)
+        max(min_salary, max_salary, salary_search)
+        if (min_salary > max_salary):
+            raise Exception
+    except Exception:
+        raise ValueError
+    else:
+        if (min_salary <= salary_search <= max_salary):
+            result = True
+        else:
+            result = False
+    return result
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
     raise NotImplementedError
 
 
