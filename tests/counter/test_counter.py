9954418a6f54fd0dc98e3095869e960be694c8e6
moisesfdasilva <msesfernandes@gmail.com>
2023-04-28 12:20:34 -0300

    test: desenvolvida o teste se a função count_ocurrences encontra uma palavra em uma arquivo, em tests/counter/test_counter.py.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    "Verifica se a palavra 'Job' aparece 3454 vezes no arquivo data/jobs.csv."
+    assert count_ocurrences(path="data/jobs.csv", word="Job") == 3454
