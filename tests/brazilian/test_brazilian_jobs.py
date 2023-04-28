f9bc9f5ca4b9de1c49bd3852ead440565fab61e4
moisesfdasilva <msesfernandes@gmail.com>
2023-04-28 13:25:57 -0300

    test: desenvolvido o teste da função test_brazilian_jobs, verificando se encontra um emprego válido, em tests/brazilian/test_brazilian_jobs.py.

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    "Verifica se a palavra 'Job' aparece 3454 vezes no arquivo data/jobs.csv."
+    output_job = {
+        'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
+    response = read_brazilian_file(path="tests/mocks/brazilians_jobs.csv")
+    assert response[0] == output_job
